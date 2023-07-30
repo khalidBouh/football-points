e2e63844822b7bac083847926633425681c7dd59
khalidBouh <khaalidm26@gmail.com>
2023-07-30 14:06:34 +0300

    Football Points Answered

--- challenge.js
+++ challenge.js
@@ -1,2 +1,8 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+function calculatePoints(wins, draws, loses){
+   return 3 * wins + 1 * draws + 0 * loses
+}
+let reselt = calculatePoints(3,2,1)
+
+console.log(`Your function should return the log ${reselt}`)
